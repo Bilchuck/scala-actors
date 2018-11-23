40f2be115e41c3f58b2afdf4b30abda14b0385f6
yostapchuk <y.ostapchuk@rickerlyman.com>
2018-11-23 16:15:23 +0200

    fix week4

--- main/src/main/scala/ua/ucu/edu/Actors.scala
+++ main/src/main/scala/ua/ucu/edu/Actors.scala
@@ -4,26 +4,4 @@ import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 
 object Actors extends App {
 
-  /* protocol */
-  case class Hey(msg: String)
-//  case object WhatsMyName
-//  case class Name(name: String)
-  case class MakeChild(name: String)
-
-  class HeyActor extends Actor with ActorLogging {
-
-    override def receive: Receive = {
-      case Hey(msg) => log.info(s"==\n==\n==Hey, $msg")
-    }
-  }
-
-  implicit val system: ActorSystem = ActorSystem("test-system")
-
-  val actor = system.actorOf(Props[HeyActor])
-
-  actor ! Hey("dude")
-
-  system.actorSelection("/user/dude")
-
-  system.terminate()
 }
